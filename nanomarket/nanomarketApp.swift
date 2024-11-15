b00d15503ad5c4d1b2a80e82f5b5a9fba9ec14d9
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2024-11-15 15:27:47 +0700

    Initial Commit

---
+++ nanomarket/nanomarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanomarketApp.swift
+//  nanomarket
+//
+//  Created by Arya Fadhil on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct nanomarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
